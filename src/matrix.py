90c8fa670f163c9a34361e8018a7cc32d9237de3
Cayden Lau <suanoo5196@gmail.com>
2020-09-09 05:54:59 +0000

    fixed variable names

--- src/matrix.py
+++ src/matrix.py
@@ -10,39 +10,39 @@ class Matrix():
     def add(self, input_matrix):
         sum_matrix = [[0, 0], [0, 0]]
 
-        for element in range(len(self.elements)):
-            for component in range(len(self.elements[0])):
-                sum_matrix[element][component] = (
-                    self.elements[element][component] + input_matrix.elements[element][component])
+        for i in range(len(self.elements)):
+            for j in range(len(self.elements[0])):
+                sum_matrix[i][j] = (
+                    self.elements[i][j] + input_matrix.elements[i][j])
 
         return Matrix(sum_matrix)
 
     def subtract(self, input_matrix):
         difference_matrix = [[0, 0], [0, 0]]
 
-        for element in range(len(self.elements)):
-            for component in range(len(self.elements[0])):
-                difference_matrix[element][component] = (
-                    self.elements[element][component] - input_matrix.elements[element][component])
+        for i in range(len(self.elements)):
+            for j in range(len(self.elements[0])):
+                difference_matrix[i][j] = (
+                    self.elements[i][j] - input_matrix.elements[i][j])
 
         return Matrix(difference_matrix)
 
     def scalar_multiply(self, input_scalar):
         rescaled_matrix = [[0, 0], [0, 0]]
 
-        for element in range(len(self.elements)):
-            for component in range(len(self.elements[0])):
-                rescaled_matrix[element][component] = self.elements[element][component] * input_scalar
+        for i in range(len(self.elements)):
+            for j in range(len(self.elements[0])):
+                rescaled_matrix[i][j] = self.elements[i][j] * input_scalar
 
         return Matrix(rescaled_matrix)
 
     def matrix_multiply(self, input_matrix):
         product_matrix = [[0, 0], [0, 0]]
 
-        for elements in range(len(self.elements)):
-            for component in range(len(input_matrix.elements[0])):
-                for factor in range(len(input_matrix.elements)):
-                    product_matrix[elements][component] += (
-                        self.elements[elements][factor] * input_matrix.elements[factor][component])
+        for i in range(len(self.elements)):
+            for j in range(len(input_matrix.elements[0])):
+                for k in range(len(input_matrix.elements)):
+                    product_matrix[i][j] += (
+                        self.elements[i][k] * input_matrix.elements[k][j])
 
         return Matrix(product_matrix)

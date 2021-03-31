780ed807fe8239d793966b19b7f373d1373a42c8
Cayden Lau <suanoo5196@gmail.com>
2021-03-31 04:15:39 +0000

    wrote gradient descent logistic regressor thing

--- src/logistic_regressor.py
+++ src/logistic_regressor.py
@@ -11,7 +11,14 @@ class LogisticRegressor:
         self.dataframe = dataframe
         self.dependent_variable = dependent_variable
         self.upper_bound = upper_bound
-        self.coefficients = self.calculate_coefficients()
+
+        try:
+            self.coefficients = self.calc_coefficients()
+
+        except:
+            coefficients = [column for column in self.dataframe.columns if column != self.dependent_variable]
+            coefficients.insert(0, 'constant')
+            self.coefficients = {element:None for element in coefficients}
 
     def calculate_coefficients(self):
         transformation = {}
@@ -29,12 +36,15 @@ class LogisticRegressor:
         prediction = 0
         columns = [variable for variable in self.dataframe.columns if variable != self.dependent_variable]
 
-        for variable in columns:
-            if variable not in input_dict and " * " not in variable:
-                input_dict[variable] = 0
+        for key in columns:
+            if key not in input_dict and " * " not in key:
+                input_dict[key] = 0
 
-            elif variable not in input_dict and " * " in variable:
-                input_dict[variable] = input_dict[variable.split(" * ")[0]] * input_dict[variable.split(" * ")[1]]
+            if key not in input_dict and " * " in key:
+                keys = key.split(" * ")
+                input_dict[key] = input_dict[keys[0]] * input_dict[keys[1]]
+
+        prediction = 0
 
         for key in self.coefficients:
             if key in input_dict:
@@ -44,3 +54,48 @@ class LogisticRegressor:
                 prediction += self.coefficients[key]
 
         return self.upper_bound / (1 + math.exp(prediction))
+
+    def copy(self):
+        return LogisticRegressor(self.dataframe, self.dependent_variable, self.upper_bound)
+
+    def calc_rss(self):  
+        rss = 0
+        independent_variables = [column for column in self.dataframe.columns if column != self.dependent_variable]
+
+        for row in self.dataframe.to_array():
+            y = row[self.dataframe.columns.index(self.dependent_variable)]
+            predict = {independent_variables[n]:row[n] for n in range(len(independent_variables))}
+            rss += (self.predict(predict) - y) ** 2
+
+        return rss
+
+    def set_coefficients(self, coeffs):
+        self.coefficients = coeffs
+
+    def calc_gradient(self, delta):
+        reg1 = self.copy()
+        reg2 = self.copy()
+        gradient = {}
+
+        for key in dict(self.coefficients):
+            grad1 = dict(self.coefficients)
+            grad2 = dict(self.coefficients)
+            grad1[key] += 0.5 * delta
+            grad2[key] -= 0.5 * delta
+            reg1.set_coefficients(grad1)
+            reg2.set_coefficients(grad2)
+            gradient[key] = (reg1.calc_rss() - reg2.calc_rss()) / delta
+
+        return gradient
+
+    def gradient_descent(self, alpha, delta, num_steps, debug_mode=False):
+        for n in range(num_steps):
+            gradient = self.calc_gradient(delta)
+            if debug_mode:
+                print("Step #{}:".format(n))
+                print("\tGradient:", gradient)
+                print("\Coefficients:", self.coefficients)
+                print("\tRSS:", self.calc_rss())
+
+            for key in gradient:
+                self.coefficients[key] -= gradient[key] * alpha
--- tests/test_logistic_regressor.py
+++ tests/test_logistic_regressor.py
@@ -3,6 +3,7 @@ sys.path.append('src')
 from matrix import Matrix
 from dataframe import DataFrame
 from logistic_regressor import LogisticRegressor
+import matplotlib.pyplot as plt
 '''
 df = DataFrame.from_array(
     [[1, 0.2],
@@ -50,7 +51,7 @@ print(log_reg.predict({
     'pb': 5,
     'beef * pb': 5*5
 }))
-'''
+
 
 df = DataFrame.from_array(
     [[0, 0, [],               1],
@@ -96,3 +97,71 @@ observation = {'beef': 8, 'mayo': 1, 'jelly': 1}
 assert round(log_reg.predict(observation), 2) == 0.79
 
 print("PASSED")
+
+#assignment 76 part (a)
+df = DataFrame.from_array(
+    [[1,0],
+    [2,0],
+    [3,0],
+    [2,1],
+    [3,1],
+    [4,1]],
+    ['x', 'y'])
+
+plt.style.use('bmh')
+plt.plot([x for x, y in df.to_array()], [y for x, y in df.to_array()],'ro')
+
+def hack(dataframe, y, delta):
+    hacky_data = []
+
+    for row in dataframe.to_array():
+        if row[dataframe.columns.index(y)] == 0:
+            row[dataframe.columns.index(y)] += delta
+
+        elif row[dataframe.columns.index(y)] == 1:
+            row[dataframe.columns.index(y)] -= delta
+
+        hacky_data.append(row)
+
+    return DataFrame.from_array(hacky_data, dataframe.columns)
+
+logistic_regressor = LogisticRegressor(hack(df, 'y', 0.1), 'y', 1)
+plt.plot([n * 0.01 for n in range(501)], [logistic_regressor.predict({'x': n * 0.01}) for n in range(501)], label='0.1')
+
+logistic_regressor2 = LogisticRegressor(hack(df, 'y', 0.01), 'y', 1)
+plt.plot([n * 0.01 for n in range(501)], [logistic_regressor2.predict({'x':n * 0.01}) for n in range(501)], label='0.01')
+
+logistic_regressor3 = LogisticRegressor(hack(df, 'y', 0.001), 'y', 1)
+plt.plot([n * 0.01 for n in range(501)], [logistic_regressor3.predict({'x': n * 0.01}) for n in range(501)], label='0.001')
+
+logistic_regressor4 = LogisticRegressor(hack(df, 'y', 0.0001), 'y', 1)
+plt.plot([n * 0.01 for n in range(501)], [logistic_regressor4.predict({'x': n * 0.01}) for n in range(501)], label='0.0001')
+plt.legend(loc='best')
+plt.savefig('hacky_logistic_regressor.png')
+
+'''
+df = DataFrame.from_array(
+    [[1,0],
+    [2,0],
+    [3,0],
+    [2,1],
+    [3,1],
+    [4,1]],
+    columns = ['x', 'y'])
+
+reg = LogisticRegressor(df, dependent_variable='y', upper_bound=1)
+
+reg.set_coefficients({'constant': 0.5, 'x': 0.5})
+
+alpha = 0.01
+delta = 0.01
+num_steps = 20000
+reg.gradient_descent(alpha, delta, num_steps)
+
+print(reg.coefficients)
+
+plt.style.use('bmh')
+plt.plot([x for x, y in df.to_array()], [y for x, y in df.to_array()],'ro')
+plt.plot([n * 0.01 for n in range(501)], [reg.predict({'x': n * 0.01}) for n in range(501)], label='Gradient descent')
+plt.legend(loc='best')
+plt.savefig('logistic_regressor_gradient_descent.png')

5ea05632034e01e228d98eb84f8c1cf2f461f55b
Cayden Lau <>
2021-05-19 03:51:29 +0000

    did elbow method

--- tests/test_k_means_clustering.py
+++ tests/test_k_means_clustering.py
@@ -1,6 +1,8 @@
 import sys
 sys.path.append('src')
 from k_means_clustering import *
+import matplotlib.pyplot as plt
+import numpy as np
 
 data = [[0.14, 0.14, 0.28, 0.44],
         [0.22, 0.1, 0.45, 0.33],
@@ -21,7 +23,7 @@ data = [[0.14, 0.14, 0.28, 0.44],
         [0.0, 0.13, 0.4, 0.49],
         [0.22, 0.07, 0.4, 0.38],
         [0.2, 0.18, 0.3, 0.4]]
-
+'''
 initial_clusters = {
     1: [0,3,6,9,12,15,18],
     2: [1,4,7,10,13,16],
@@ -35,3 +37,37 @@ print("Clusters:", kmeans.clusters , "\n")
 
 print("Centers:", kmeans.centers, "\n")
 
+'''
+k_values = [n for n in range(1, 6)]
+distortions = []
+
+
+def get_clusters(k):
+    clusters = {n:[] for n in range(1, k + 1)}
+
+    for n in range(len(data)):
+        clusters[(n % k) + 1].append(n)
+
+    return clusters
+
+def get_distortion(clusters, centers):
+    distortion = 0
+
+    for center in centers:
+
+        for cluster in clusters[center]:
+            distortion += np.linalg.norm(np.array(centers[center]) - np.array(data[cluster])) ** 2
+
+    return distortion
+
+for k in k_values:
+    kmeans = KMeans(get_clusters(k), data)
+    kmeans.run()
+    distortions.append(get_distortion(kmeans.clusters, kmeans.centers))
+
+
+plt.style.use('bmh')
+plt.plot(k_values, distortions)
+plt.xlabel('k')
+plt.ylabel('sum squared accuracy')
+plt.savefig('elbow_method.png')

9b0f102dbc172e7fbfabc676060778842099f065
Cayden Lau <69777457+RavenHex1296@users.noreply.github.com>
2021-10-01 22:08:08 +0000

    stuff

--- decision_tree.py
+++ decision_tree.py
@@ -1,23 +1,25 @@
 import math
-'''
-class Point():
-    def __init__(self, point_type, location):
-        self.point_type = point_type
-        self.location = location
-'''
+import random
+
 class DecisionTree():
-    def __init__(self, data):
-        self.point_data = data
-        self.set_points(data)
-        self.possible_splits = self.get_all_possible_splits()
-        self.best_split = self.get_best_split()
+    def __init__(self, data_dict, min_size_to_split):
+        self.point_data = data_dict
+        self.entropy = self.calculate_entropy()
         self.branches = []
-'''
-    def set_points(self, data_dict):
+        self.best_split = None
+        self.min_size_to_split = min_size_to_split
+
+    def get_all_points(self, data_dict=None):
+        all_points = []
+
+        if data_dict == None:
+            data_dict = self.point_data
+
         for key in data_dict:
-            for point in point_data[key]:
-                point = Point(key, point)
-'''
+            all_points += data_dict[key]
+
+        return all_points
+
     def remove_from_dict(self, input_dict, values):
         new_dict = input_dict.copy()
 
@@ -33,73 +35,157 @@ class DecisionTree():
 
                 new_values.append(value)
 
-            new_dict[key] = new_items
+            new_dict[key] = new_values
 
         return new_dict
 
-    def get_all_points(self, data_dict):
-        all_points = []
-
-        for key in data_dict:
-            all_points += data_dict[key]
-
-        return all_points
-
-    def calculate_entropy(self, data_dict):
+    def calculate_entropy(self, data_dict=None):
         entropy = 0
 
+        if data_dict == None:
+            data_dict = self.point_data
+
         for point_type in data_dict:
-            p = len(data_dict[point_type]) / len(self.get_all_points(data_dict))
-            entropy += -1 * p * math.log(p, math.e)
+            proportion = len(data_dict[point_type]) / len(self.get_all_points(data_dict))
+
+            if proportion == 0:
+                continue
+
+            entropy += -1 * proportion * math.log(proportion)
 
         return entropy
 
-    def calculate_weighted_average(self, data_dict):
-        num_values = len(self.get_all_points(data_dict))
-        #have to figure out how to actually carry out the split
+    def calculate_weighted_average(self, data):
+        weighted_average = 0
+        num_points = sum([len(self.get_all_points(data_dict)) for data_dict in data])
 
-    def get_all_unique_x_values(self):
-        x_vales = [data[0] for data in self.get_all_points() if data[0] not in x_values]
-        return x_values
+        for data_dict in data:
+            weighted_average += self.calculate_entropy(data_dict) * len(self.get_all_points(data_dict)) / num_points
 
-    def get_all_unique_y_values(self):
-        y_values = [data[1] for data in self.get_all_points() if data[1] not in y_values]
-        return y_values
+        return weighted_average
 
-    def get_all_possible_splits(self):
-        possible_splits = {0: [], 1: []}
+    def calculate_splits(self, unique_values):
+        midpoints = []
 
-        x_values = self.get_all_unique_x_values().sort()
-        y_values = self.get_all_unique_y_values().sort()
+        for n in range(len(unique_values) - 1):
+            midpoints.append((unique_values[n] + unique_values[n + 1]) / 2)
 
-        for n in range(len(x_values) - 1):
-            possible_splits[0].append((x_values[n] + x_values[n + 1]) / 2)
-            possible_splits[1].append((y_values[n] + y_values[n + 1]) / 2)
+        return midpoints
+
+    def split(self, split_info, data_dict=None):
+        set_branches = False
+
+        if data_dict == None:
+            set_branches = True
+            data_dict = self.point_data
+
+        above = [point for point in self.get_all_points(data_dict) if point[split_info[0]] >= split_info[1]]
+        below = [point for point in self.get_all_points(data_dict) if point[split_info[0]] < split_info[1]]
+
+        above_midpoint = self.remove_from_dict(data_dict, below)
+        below_midpoint = self.remove_from_dict(data_dict, above)
+
+        if set_branches:
+            self.branches = [DecisionTree(above_midpoint, self.min_size_to_split), DecisionTree(below_midpoint, self.min_size_to_split)]
+
+        return [above_midpoint, below_midpoint]
+
+    def get_all_possible_splits(self, data_dict=None):
+        possible_splits = []
+
+        if data_dict == None:
+            data_dict = self.point_data
+
+        all_points = self.get_all_points(data_dict)
+
+        for n in range(len(all_points[0])):
+            unique = list(set([point[n] for point in all_points]))
+            midpoints = self.calculate_splits(unique)
+
+            for midpoint in midpoints:
+                possible_splits.append((n, midpoint))
 
         return possible_splits
 
-    def split(self, split_info, data_dict):
-        above_midpoint = [point for point  in self.get_all_points(data_dict) if point[split_tuple[0]] >= split_tuple[1]]
-        below_midpoint = [point for point in self.get_all_points(data_dict) if point[split_tuple[0]] < split_tuple[1]]
+    def get_best_split(self, data_dict=None):
+        set_best_split = False
 
-        above_dict = self.remove_from_dict(point_dict, lesser)
-        below_dict = self.remove_from_dict(point_dict, greater)
+        if data_dict == None:
+            set_best_split = True
+            data_dict = self.point_data
 
-        return [DecisionTree(above_dict), DecisionTree(below_dict)]
+        all_splits = self.get_all_possible_splits(data_dict)
+        best_split = all_splits[0]
 
-    def get_best_split():
-        for key in self.possible_splits():
-            for n in range(0, len(self.get_all_possible_splits[key])):
-                midpoint = self.get_all_possible_splits[key][n]
+        best_entropy = self.calculate_weighted_average(self.split(best_split, data_dict))
 
-                above_midpoint = []
-                below_midpoint = []
+        for split in all_splits:
+            branches = self.split(split, data_dict)
+            weighted_entropy = self.calculate_weighted_average(branches)
 
-                for point in self.get_all_points(self.get_all_points):
-                    if point[key] > midpoint:
-                        above_midpoint.append(point)
+            if weighted_entropy < best_entropy:
+                best_split = split
+                best_entropy = weighted_entropy
 
-                    if point[key] < midpoint:
-                        below_midpoint.append(point)
+        if set_best_split:
+            self.best_split = best_split
 
-                for point in above_midpoint:
+        return best_split
+
+    def fit(self, decision_tree=None):
+        if decision_tree == None:
+            decision_tree = self
+
+        if len(decision_tree.branches) != 0 or len(decision_tree.get_all_points()) <= self.min_size_to_split:
+            return
+
+        decision_tree.get_best_split()
+        decision_tree.split(decision_tree.best_split)
+        branches = [branch for branch in decision_tree.branches if branch.entropy != 0]
+        next_branches = []
+
+        while True:
+            for branch in branches:
+                if len(branch.get_all_points()) <= self.min_size_to_split or len(set(branch.get_all_points())) == 1:
+                    continue
+
+                branch.get_best_split()
+                branch.split(branch.best_split)
+                next_branches += [next_branch for next_branch in branch.branches if next_branch.entropy != 0]
+
+            branches = next_branches
+            next_branches = []
+
+            if len(branches) == 0:
+                break
+
+    def get_point_type(self, decision_tree=None):
+        if decision_tree == None:
+            decision_tree = self
+
+        if decision_tree.entropy != 0:
+            sample_sizes = [(key, len(decision_tree.point_data[key])) for key in decision_tree.point_data]
+            majority = max([sample_size[1] for sample_size in sample_sizes])
+            new_lengths = [sample_size for sample_size in sample_sizes if sample_size[1] == majority]
+
+            if len(new_lengths) > 1:
+                return new_lengths[random.randint(0, len(new_lengths) - 1)][0]
+
+            return new_lengths[0][0]
+
+        point_type = [key for key in decision_tree.point_data.keys() if len(decision_tree.point_data[key]) != 0]
+        return point_type[0]
+
+    def predict(self, unknown_point, decision_tree=None):
+        if decision_tree == None:
+            decision_tree = self
+
+        while decision_tree.entropy != 0 and len(decision_tree.get_all_points()) > self.min_size_to_split and len(set(decision_tree.get_all_points())) != 1:
+
+            if unknown_point[decision_tree.best_split[0]] >= decision_tree.best_split[1]:
+                decision_tree = decision_tree.branches[0]
+
+            elif unknown_point[decision_tree.best_split[0]] < decision_tree.best_split[1]:
+                decision_tree = decision_tree.branches[1]
+
+        return decision_tree.get_point_type()
--- test_decision_tree.py
+++ test_decision_tree.py
@@ -1,12 +1,81 @@
-from decision_tree import *
 
+from decision_tree import *
+import math
+import random
+'''
 data = {'x': [(2, 1), (2, 2), (3, 2), (3, 3)], 'o': [(2, 3), (2, 4), (3, 4)]}
 
-decision_tree = DecisionTree(dataset)
+decision_tree = DecisionTree(data)
+assert decision_tree.point_data == data
+assert round(decision_tree.entropy, 3) == 0.683
+assert decision_tree.get_best_split() == (1, 2.5)
 
-
+decision_tree.fit()
+assert decision_tree.predict((0, -1)) == 'x'
+assert decision_tree.predict((5, 6)) == 'o'
+assert decision_tree.predict((2, 3)) == 'o'
+assert decision_tree.predict((5, 3)) == 'x'
 
 
 data = {'x': [(1, 3), (1, 2), (2, 2)], 'o': [(1, 1), (2, 1), (3, 1), (3, 2)]}
 
-decision_tree = DecisionTree(dataset)
+decision_tree = DecisionTree(data)
+assert decision_tree.point_data == data
+assert round(decision_tree.entropy, 3) == 0.683
+assert decision_tree.get_best_split() == (1, 1.5)
+
+decision_tree.fit()
+assert decision_tree.predict((100, 100)) == 'o'
+assert decision_tree.predict((-10, -10)) == 'o'
+assert decision_tree.predict((1, 3)) == 'x'
+
+print('PASSED')
+
+data = {'x': [(1, 7), (2, 7), (3, 7), (3, 8), (3, 9), (7, 1)], 'o': [(1, 9), (5, 3), (6, 3), (7, 3), (5, 2), (5, 1)]}
+decision_tree = DecisionTree(data, 7)
+decision_tree.fit()
+
+assert decision_tree.predict((2, 8)) == 'x'
+assert decision_tree.predict((6, 2)) == 'o'
+assert decision_tree.predict((-10, 2)) == 'x'
+assert decision_tree.predict((100, 2)) == 'o'
+assert decision_tree.predict((4, 5)) == 'o'
+assert len(decision_tree.branches[0].branches) == 0
+assert len(decision_tree.branches[1].branches) == 0
+
+data = {'x': [(1, 5), (2, 5), (1, 3), (2, 4)], 'o': [(1, 4), (2, 3)]}
+
+decision_tree = DecisionTree(data, 5)
+decision_tree.fit()
+assert decision_tree.predict((2, 8)) == 'x'
+assert decision_tree.predict((-10, 100)) == 'x'
+
+random.seed(0)
+initial_prediction = decision_tree.predict((-2, -5))
+
+for n in range(-10, 4):
+    random.seed(0)
+    assert decision_tree.predict((0, n)) == initial_prediction
+
+assert decision_tree.predict((2, 5)) == 'x'
+assert decision_tree.predict((0, 10)) == 'x'
+assert len(decision_tree.branches[0].branches) == 0
+assert len(decision_tree.branches[1].branches) == 0
+'''
+data = {'x': [(0, 1), (0, 1), (0, 2), (1, 1), (1, 2), (1, 2)], 'o': [(0, 2), (1, 1), (1, 1), (1, 2)]}
+decision_tree = DecisionTree(data, 1)
+decision_tree.fit()
+
+assert decision_tree.predict((3, 5)) == 'x'
+assert decision_tree.predict((0, 0)) == 'x'
+assert decision_tree.predict((3, 1)) == 'o'
+
+random.seed(0)
+initial_prediction = decision_tree.predict((0, 4))
+
+for n in range(-10, 0):
+    for num in range(2, 12):
+        random.seed(0)
+        assert decision_tree.predict((n, num)) == initial_prediction
+
+print('PASSED')

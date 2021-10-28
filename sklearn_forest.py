b0cb9f56fbc31fc3c7e3a258d6517464503141a7
RavenHex1296 <suanoo5196@gmail.com>
2021-10-28 16:43:14 +0000

    2500 tests

--- sklearn_forest.py
+++ sklearn_forest.py
@@ -10,26 +10,12 @@ import sys
 sys.path.append('datasets')
 from semi_random_data import *
 import matplotlib.pyplot as plt
+import time
 
-point_data = get_semi_random_data(200)
-x_values = []
-y_values = []
-point_types = []
 
-for point_type in point_data:
-    for point in point_data[point_type]:
-        point_types.append(point_type)
-        x_values.append(point[0])
-        y_values.append(point[1])
-
-point_data = {'x_value': x_values, 'y_value': y_values, 'point_type': point_types}
-
-point_data = pd.DataFrame.from_dict(point_data)
-X = point_data[['x_value', 'y_value']]
-y = point_data['point_type']
-accuracies = []
 
 '''
+accuracies = []
 min_size_to_split_values = [2, 5, 10, 15, 20, 30, 50, 100, 200, 250, 300]
 
 for min_size_to_split in min_size_to_split_values:
@@ -45,8 +31,6 @@ plt.xlabel('min_size_to_split')
 plt.ylabel('5-fold Cross Valiation Accuracy')
 plt.savefig('sklearn_dt_plot.png')
 
-'''
-
 num_trees = [1, 10, 20, 50, 100, 500, 1000]
 
 for num_tree in num_trees:
@@ -61,3 +45,47 @@ plt.plot(num_trees, accuracies)
 plt.xlabel('num_trees')
 plt.ylabel('5-fold Cross Valiation Accuracy')
 plt.savefig('sklearn_rf_plot.png')
+
+'''
+
+start_time = time.time()
+point_data = get_semi_random_data(200)
+point_data_dict = point_data.copy()
+x_values = []
+y_values = []
+point_types = []
+
+for point_type in point_data:
+    for point in point_data[point_type]:
+        point_types.append(point_type)
+        x_values.append(point[0])
+        y_values.append(point[1])
+
+point_data = {'x_value': x_values, 'y_value': y_values, 'point_type': point_types}
+
+point_data = pd.DataFrame.from_dict(point_data)
+X = point_data[['x_value', 'y_value']]
+y = point_data['point_type']
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5)
+rf = RandomForestClassifier(n_estimators=100)
+rf.fit(X_train,y_train)
+
+
+test_points = {'x_value': [random.uniform(-6, 6) for _ in range(2500)], 'y_value': [random.uniform(-6, 6) for _ in range(2500)]}
+test_point_predictions = {'x': [], 'o': []}
+test_points = pd.DataFrame.from_dict(test_points)
+predicted_point_types = rf.predict(test_points)
+
+for n in range(0, len(predicted_point_types)):
+    x = test_points['x_value'][n]
+    y = test_points['y_value'][n]
+    test_point_predictions[predicted_point_types[n]].append((x, y))
+
+plt.style.use('bmh')
+plt.scatter([point[0] for point in test_point_predictions['x']], [point[1] for point in test_point_predictions['x']], s=25,c='lightcoral')
+plt.scatter([point[0] for point in test_point_predictions['o']], [point[1] for point in test_point_predictions['o']], s=25,c='lightblue')
+plt.scatter([point[0] for point in point_data_dict['x']], [point[1] for point in point_data_dict['x']], s=75, c='red')
+plt.scatter([point[0] for point in point_data_dict['o']], [point[1] for point in point_data_dict['o']], s=75,c='blue')
+plt.savefig('rf.png')
+print(time.time() - start_time)
